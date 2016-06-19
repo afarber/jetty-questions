9b2cf070327c3c4bd32992ea18091b02813b9650
Alexander Farber <Alexander.Farber@gmail.com>
2016-06-19 20:55:49 +0200

    Add systemd service; allow binding to specific IP address and port

--- WebsocketHandler/src/main/java/de/afarber/MyHandler.java
+++ WebsocketHandler/src/main/java/de/afarber/MyHandler.java
@@ -1,22 +1,34 @@
 package de.afarber;
 
+import java.net.InetSocketAddress;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.websocket.server.WebSocketHandler;
 import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
 
 @SuppressWarnings("serial")
 public class MyHandler extends WebSocketHandler {
+    private final static Pattern PATTERN = Pattern.compile("^([0-9.]+):([0-9]+)$"); 
+    
     @Override
     public void configure(WebSocketServletFactory factory) {
-        //factory.getPolicy().setIdleTimeout(10 * 60 * 1000);     // 10 minutes
-        //factory.getPolicy().setMaxTextMessageSize(1 * 1024);    // 1 KByte
-        //factory.getPolicy().setMaxTextMessageSize(64 * 1024);   // 64 KByte
-        
         factory.register(MyListener.class);
     }
     
-    public static void main(String[] args) throws Exception {
-        Server server = new Server(8080);
+   public static void main(String[] args) throws Exception {
+        String host = "127.0.0.1";
+        int port = 8080;
+        
+        if (args.length > 0) {
+            Matcher matcher = PATTERN.matcher(args[0]);
+            if (matcher.matches()) {
+                host = matcher.group(1);
+                port = Integer.parseInt(matcher.group(2));
+            }
+        }
+        
+        Server server = new Server(new InetSocketAddress(host, port));
         server.setHandler(new MyHandler());
         server.start();
         server.join();
