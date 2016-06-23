17fbf0b89f7a59a6e8ef5ee1721e19acecbea06e
Alexander Farber <Alexander.Farber@gmail.com>
2016-06-23 16:47:32 +0200

    Fix NPE by adding HttpConnectionFactory as shown in examples/embedded/src/main/java/org/eclipse/jetty/embedded/LikeJettyXml.java

--- WssClient/util_win.bat
+++ WssClient/util_win.bat
@@ -3,6 +3,8 @@
 chcp 65001
 
 set JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF8
+set JETTY_DEBUG=-Dorg.eclipse.jetty.LEVEL=DEBUG -Djavax.net.debug=ssl
+set JETTY_DEBUG=
 set REPO="C:\Users\user1\.m2\repository"
 set VERSION=9.3.10.v20160621
 
@@ -13,5 +15,5 @@ set CPATHS=%CPATHS%;%REPO%\org\eclipse\jetty\websocket\websocket-api\%VERSION%\w
 set CPATHS=%CPATHS%;%REPO%\org\eclipse\jetty\websocket\websocket-client\%VERSION%\websocket-client-%VERSION%.jar
 set CPATHS=%CPATHS%;%REPO%\org\eclipse\jetty\websocket\websocket-common\%VERSION%\websocket-common-%VERSION%.jar
 
-"C:\Program Files\Java\jdk1.8.0_66\bin\java.exe" -Djavax.net.debug=ssl -cp %CPATHS% de.afarber.wssclient.Main %*
+"C:\Program Files\Java\jdk1.8.0_66\bin\java.exe" %JETTY_DEBUG% -cp %CPATHS% de.afarber.wssclient.Main %*
 
--- WssEmbedded/src/main/java/de/afarber/wssembedded/MyHandler.java
+++ WssEmbedded/src/main/java/de/afarber/wssembedded/MyHandler.java
@@ -1,6 +1,9 @@
 package de.afarber.wssembedded;
 
 import org.eclipse.jetty.http.HttpVersion;
+import org.eclipse.jetty.server.HttpConfiguration;
+import org.eclipse.jetty.server.HttpConnectionFactory;
+import org.eclipse.jetty.server.SecureRequestCustomizer;
 import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.ServerConnector;
 import org.eclipse.jetty.server.SslConnectionFactory;
@@ -24,6 +27,18 @@ public class MyHandler extends WebSocketHandler {
         Server server = new Server();
         server.setHandler(new MyHandler());
         
+        HttpConfiguration http_config = new HttpConfiguration();
+        http_config.setSecureScheme("https");
+        http_config.setSecurePort(WSS_PORT);
+        http_config.setOutputBufferSize(32768);
+        http_config.setRequestHeaderSize(8192);
+        http_config.setResponseHeaderSize(8192);
+        http_config.setSendServerVersion(true);
+        http_config.setSendDateHeader(false);
+        
+        HttpConfiguration https_config = new HttpConfiguration(http_config);
+        https_config.addCustomizer(new SecureRequestCustomizer());
+        
        // keytool -v -genkeypair -alias key1 -keyalg RSA -keysize 2048 -validity 3650 -keypass changeit -keystore keystore.jks -storepass changeit
        // java -cp "jetty-util\9.3.10.v20160621\*" org.eclipse.jetty.util.security.Password changeit
        
@@ -37,7 +52,9 @@ public class MyHandler extends WebSocketHandler {
         server.addConnector(wsConnector);
         
         ServerConnector wssConnector = new ServerConnector(server,
-            new SslConnectionFactory(sslContextFactory, HttpVersion.HTTP_1_1.asString()));
+            new SslConnectionFactory(sslContextFactory, HttpVersion.HTTP_1_1.asString()),
+            new HttpConnectionFactory(https_config));
+        
         wssConnector.setHost(HOST);
         wssConnector.setPort(WSS_PORT);
         server.addConnector(wssConnector);
