c7d0614fbe858212677ee7c90af66974ea4ffe02
Alexander Farber <Alexander.Farber@gmail.com>
2016-07-04 07:44:47 +0200

    Add WebSocket annotations

--- WebSocketWarApp/src/main/java/de/afarber/EchoListener.java
+++ WebSocketWarApp/src/main/java/de/afarber/EchoListener.java
@@ -4,34 +4,45 @@ import org.eclipse.jetty.util.log.Log;
 import org.eclipse.jetty.util.log.Logger;
 import org.eclipse.jetty.websocket.api.Session;
 import org.eclipse.jetty.websocket.api.WebSocketListener;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketClose;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketConnect;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketError;
+import org.eclipse.jetty.websocket.api.annotations.OnWebSocketMessage;
+import org.eclipse.jetty.websocket.api.annotations.WebSocket;
 
+@WebSocket
 public class EchoListener implements WebSocketListener {
     private static final Logger LOG = Log.getLogger(EchoListener.class);
     private Session mSession;
 
     @Override
+    @OnWebSocketMessage
     public void onWebSocketBinary(byte[] payload, int offset, int len) {
         LOG.info("onWebSocketBinary");
     }
 
     @Override
+    @OnWebSocketClose
     public void onWebSocketClose(int statusCode, String reason) {
         LOG.info("onWebSocketClose {} {}", statusCode, reason);
         mSession = null;
     }
 
     @Override
+    @OnWebSocketConnect
     public void onWebSocketConnect(Session session) {
         LOG.info("onWebSocketConnect {}", session);
         mSession = session;
     }
 
     @Override
+    @OnWebSocketError
     public void onWebSocketError(Throwable cause) {
         LOG.warn("onWebSocketError {}", cause);
     }
 
     @Override
+    @OnWebSocketMessage
     public void onWebSocketText(String message) {
         LOG.info("onWebSocketText {}", message);
         if (mSession != null && mSession.isOpen()) {
--- WebSocketWarApp/src/main/java/de/afarber/WsServlet.java
+++ WebSocketWarApp/src/main/java/de/afarber/WsServlet.java
@@ -1,8 +1,11 @@
 package de.afarber;
 
+import javax.servlet.annotation.WebServlet;
 import org.eclipse.jetty.websocket.servlet.WebSocketServlet;
 import org.eclipse.jetty.websocket.servlet.WebSocketServletFactory;
 
+@SuppressWarnings("serial")
+@WebServlet(name = "WsServlet", urlPatterns = { "/ws" })
 public class WsServlet extends WebSocketServlet
 {
     @Override
